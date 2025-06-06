bf4e07437de2532295843d3b49f6130e29898b96
Laylagnes <laylaagnes001@gmail.com>
2025-06-06 16:31:56 -0300

    Update menu-responsivo.js

--- menu-responsivo.js
+++ menu-responsivo.js
@@ -6,8 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const expanded = this.getAttribute('aria-expanded') === 'true';
     this.setAttribute('aria-expanded', !expanded);
     menu.classList.toggle('show');
-
-     // Trocar o ícone do botão
     this.textContent = expanded ? '☰' : '✖';
+  });
+
+  const menuLinks = menu.querySelectorAll('a');
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      // Fecha o menu
+      menu.classList.remove('show');
+      toggleBtn.setAttribute('aria-expanded', false);
+      toggleBtn.textContent = '☰'; // Ícone do menu fechado
+    });
   });
 });
